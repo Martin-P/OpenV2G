8b7ef066d06ccd06f61fa1247494b154398cdcdb
sebastiankb <sebastiankb@d9f2db14-54d0-4bde-b00c-16405c910529>
2011-12-02 09:53:19 +0000

    release 0.5

---
+++ Release/src/transport/subdir.mk
@@ -0,0 +1,24 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../src/transport/v2gtp.c 
+
+OBJS += \
+./src/transport/v2gtp.o 
+
+C_DEPS += \
+./src/transport/v2gtp.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/transport/%.o: ../src/transport/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C Compiler'
+	gcc -I"D:\Data\MCHN4240\workspace_c\OpenV2G\trunk\src\codec" -I"D:\Data\MCHN4240\workspace_c\OpenV2G\trunk\src\transport" -I"D:\Data\MCHN4240\workspace_c\OpenV2G\trunk\src\service" -I"D:\Data\MCHN4240\workspace_c\OpenV2G\trunk\src\test" -O3 -Os -pedantic -pedantic-errors -Wall -c -fmessage-length=0 -ansi -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o"$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
