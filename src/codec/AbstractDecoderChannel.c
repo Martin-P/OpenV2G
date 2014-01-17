0933696169f69036cc0af7e6b58ed329735a09cc
daniel_peintner <daniel_peintner@d9f2db14-54d0-4bde-b00c-16405c910529>
2014-01-17 07:56:21 +0000

    git-svn-id: https://svn.code.sf.net/p/openv2g/code/trunk@81 d9f2db14-54d0-4bde-b00c-16405c910529

--- src/codec/AbstractDecoderChannel.c
+++ src/codec/AbstractDecoderChannel.c
@@ -16,6 +16,7 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
+ 
 /*******************************************************************
  *
  * @author Daniel.Peintner.EXT@siemens.com
