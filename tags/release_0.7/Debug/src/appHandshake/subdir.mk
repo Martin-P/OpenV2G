0257eccbcd974e27c133ac805f290bc1e4ebd553
sebastiankb <sebastiankb@d9f2db14-54d0-4bde-b00c-16405c910529>
2014-04-15 13:37:30 +0000

    Tag release OpenV2G 0.7

---
+++ tags/release_0.7/Debug/src/appHandshake/subdir.mk
@@ -0,0 +1,30 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../src/appHandshake/appHand_DataDeserialization.c \
+../src/appHandshake/appHand_DataSerialization.c \
+../src/appHandshake/appHand_dataTypes.c 
+
+OBJS += \
+./src/appHandshake/appHand_DataDeserialization.o \
+./src/appHandshake/appHand_DataSerialization.o \
+./src/appHandshake/appHand_dataTypes.o 
+
+C_DEPS += \
+./src/appHandshake/appHand_DataDeserialization.d \
+./src/appHandshake/appHand_DataSerialization.d \
+./src/appHandshake/appHand_dataTypes.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/appHandshake/%.o: ../src/appHandshake/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C Compiler'
+	gcc -I"D:\Data\MCHN4240\workspace_c\OpenV2G\trunk\src\codec" -I"D:\Data\MCHN4240\workspace_c\OpenV2G\trunk\src\appHandshake" -I"D:\Data\MCHN4240\workspace_c\OpenV2G\trunk\src\codec\appHandCodec" -I"D:\Data\MCHN4240\workspace_c\OpenV2G\trunk\src\transport" -I"D:\Data\MCHN4240\workspace_c\OpenV2G\trunk\src\service" -I"D:\Data\MCHN4240\workspace_c\OpenV2G\trunk\src\test" -O0 -g3 -pedantic -pedantic-errors -Wall -c -fmessage-length=0 -ansi -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o"$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
---
+++ tags/release_0.7/Release/src/codec/appHandCodec/subdir.mk
@@ -0,0 +1,30 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../src/codec/appHandCodec/appHand_EXIDecoder.c \
+../src/codec/appHandCodec/appHand_EXIEncoder.c \
+../src/codec/appHandCodec/appHand_NameTableEntries.c 
+
+OBJS += \
+./src/codec/appHandCodec/appHand_EXIDecoder.o \
+./src/codec/appHandCodec/appHand_EXIEncoder.o \
+./src/codec/appHandCodec/appHand_NameTableEntries.o 
+
+C_DEPS += \
+./src/codec/appHandCodec/appHand_EXIDecoder.d \
+./src/codec/appHandCodec/appHand_EXIEncoder.d \
+./src/codec/appHandCodec/appHand_NameTableEntries.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/codec/appHandCodec/%.o: ../src/codec/appHandCodec/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C Compiler'
+	gcc -I"D:\Data\MCHN4240\workspace_c\OpenV2G\trunk\src\codec" -I"D:\Data\MCHN4240\workspace_c\OpenV2G\trunk\src\codec\appHandCodec" -I"D:\Data\MCHN4240\workspace_c\OpenV2G\trunk\src\appHandshake" -I"D:\Data\MCHN4240\workspace_c\OpenV2G\trunk\src\transport" -I"D:\Data\MCHN4240\workspace_c\OpenV2G\trunk\src\service" -I"D:\Data\MCHN4240\workspace_c\OpenV2G\trunk\src\test" -O3 -Os -pedantic -pedantic-errors -Wall -c -fmessage-length=0 -ansi -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o"$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
