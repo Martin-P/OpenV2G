cb332b4d251f9b82dcd4282a24a3b9b24947d71b
uhi22 <uhi@hnng.de>
2022-11-08 13:13:03 +0100

    included decoder result in json. decoding of both direction of handshake works.

--- src/test/main_commandlineinterface.c
+++ src/test/main_commandlineinterface.c
@@ -57,6 +57,7 @@ int g_errn;
 char gResultString[4096];
 char gInfoString[4096];
 char gErrorString[4096];
+char s[1000];
 
 
 #define ERROR_UNEXPECTED_REQUEST_MESSAGE -601
@@ -90,10 +91,13 @@ static int writeStringToEXIString(char* string, exi_string_character_t* exiStrin
 
 static void printASCIIString(exi_string_character_t* string, uint16_t len) {
 	unsigned int i;
+	char strShort[10];
+	strcpy(s, "");
 	for(i=0; i<len; i++) {
-		printf("%c",(char)string[i]);
+		sprintf(strShort, "%c",(char)string[i]);
+		strcat(s, strShort);
 	}
-	printf("\n");
+	//printf("\n");
 }
 
 
@@ -163,6 +167,19 @@ static void encodeSupportedAppProtocolResponse(void) {
   sprintf(gInfoString, "encodeSupportedAppProtocolResponse finished");
 }
 
+static void encodeSessionSetupRequest(void) {
+	struct dinEXIDocument dinDoc;
+	dinDoc.V2G_Message_isUsed = 1u;
+	init_dinMessageHeaderType(&dinDoc.V2G_Message.Header);
+	init_dinBodyType(&dinDoc.V2G_Message.Body);
+	dinDoc.V2G_Message.Body.SessionSetupReq_isUsed = 1u;
+	init_dinSessionSetupReqType(&dinDoc.V2G_Message.Body.SessionSetupReq);
+	prepareGlobalStream();
+	g_errn = encode_dinExiDocument(&global_stream1, &dinDoc);
+    printGlobalStream();
+    sprintf(gInfoString, "encodeSessionSetupRequest finished");
+}
+
 static void encodeSessionSetupResponse(void) {
 	struct dinEXIDocument dinDoc;
 	dinDoc.V2G_Message_isUsed = 1u;
@@ -190,12 +207,14 @@ static void encodeSessionSetupResponse(void) {
 	prepareGlobalStream();
 	g_errn = encode_dinExiDocument(&global_stream1, &dinDoc);
     printGlobalStream();
+    sprintf(gInfoString, "encodeSessionSetupResponse finished");
 }
 
 void encodeServiceDiscoveryResponse(void) {
 	struct dinEXIDocument dinDoc;
 	dinDoc.V2G_Message_isUsed = 1u;
 	sprintf(gErrorString, "todo encodeServiceDiscoveryResponse");
+    sprintf(gInfoString, "encodeServiceDiscoveryResponse finished");
 }
 void encodePaymentServiceSelectionResponse(void) {
 	sprintf(gErrorString, "todo encodePaymentServiceSelectionResponse");
@@ -233,7 +252,7 @@ static int runTheEncoder(char* parameterStream) {
 	case 'D':
 		switch (parameterStream[2]) {
 			case 'A':
-				sprintf(gErrorString, "encodeSessionSetupRequest  not yet implemented.");
+				encodeSessionSetupRequest();
 				break;
 			case 'a':
 				encodeSessionSetupResponse();
@@ -302,7 +321,8 @@ static int runTheDecoder(char* parameterStream) {
 	char strOneByteHex[3];
 	int schema;
 
-	if (strlen(parameterStream)<3) {
+	if (strlen(parameterStream)<4) {
+		/* minimum is 4 characters, e.g. DH01 */
 		sprintf(gErrorString, "parameter too short");
 		return -1;
 	}
@@ -315,6 +335,13 @@ static int runTheDecoder(char* parameterStream) {
 		The caller needs to choose, depending on its context-knowledge, the correct decoder.
 		The first step in a session is always to use the decode_appHandExiDocument for finding out, which specification/decoder
 		is used in the next steps. */
+		
+	if (parameterStream[1]=='h') {
+		/* for the decoder, it does not matter whether it is a handshake request (H) or handshake response (h).
+		   The same decoder schema is used. */
+		parameterStream[1] = 'H';
+	}
+	
 	if (parameterStream[1]=='H') {
 		schema = SCHEMA_APP_HANDSHAKE;
 	} else if (parameterStream[1]=='D') {
@@ -325,15 +352,15 @@ static int runTheDecoder(char* parameterStream) {
 		schema = SCHEMA_ISO2;
 	} else {
 		sprintf(gErrorString, "The second character of the parameter must be H for applicationHandshake, D for DIN, 1 for ISO1 or 2 for ISO2.");
-		schema = -1;
+		return -1;
+		//schema = -1;
 	}
 	global_stream1.size = BUFFER_SIZE;
 	global_stream1.data = buffer1;
 	global_stream1.pos = &global_pos1;
 	//printf("decoderTest for %s\n", parameterStream);
-	numBytes=strlen(parameterStream)/2;
+	numBytes=strlen(parameterStream)/2; /* contains one "virtual byte e.g. DH" at the beginning, which does not belong to the payload. */
 	global_pos1 = 0;
-	printf("numBytes %d\n", numBytes);
 	strOneByteHex[2]=0;
 	/* convert the hex stream into array of bytes: */
 	for (i=1; i<numBytes; i++) { /* starting at 1, means the first two characters (the direction-and-schema-selectors) are jumped-over. */
@@ -341,6 +368,7 @@ static int runTheDecoder(char* parameterStream) {
 		strOneByteHex[1] = parameterStream[2*i+1];
 		buffer1[global_pos1++] = strtol(strOneByteHex, NULL, 16);
 	}
+	sprintf(gInfoString, "%d bytes to convert", global_pos1);
 	/*
 	printf("size = %d\n", global_stream1.size);
 	printf("pos  = %d\n", *(global_stream1.pos));
@@ -374,14 +402,36 @@ static int runTheDecoder(char* parameterStream) {
 	}		
 
 	if (schema == SCHEMA_APP_HANDSHAKE) {
-		printf("EVSE side: List of application handshake protocols of the EV \n");
-		for(i=0;i<exiDocAppHand.supportedAppProtocolReq.AppProtocol.arrayLen;i++) {
-			printf("\tProtocol entry #=%d\n",(i+1));
-			printf("\t\tProtocolNamespace=");
-			printASCIIString(exiDocAppHand.supportedAppProtocolReq.AppProtocol.array[i].ProtocolNamespace.characters, exiDocAppHand.supportedAppProtocolReq.AppProtocol.array[i].ProtocolNamespace.charactersLen);
-			printf("\t\tVersion=%d.%d\n", exiDocAppHand.supportedAppProtocolReq.AppProtocol.array[i].VersionNumberMajor, exiDocAppHand.supportedAppProtocolReq.AppProtocol.array[i].VersionNumberMinor);
-			printf("\t\tSchemaID=%d\n", exiDocAppHand.supportedAppProtocolReq.AppProtocol.array[i].SchemaID);
-			printf("\t\tPriority=%d\n", exiDocAppHand.supportedAppProtocolReq.AppProtocol.array[i].Priority);
+		if (exiDocAppHand.supportedAppProtocolReq_isUsed) {
+			/* it is a request */
+			/* EVSE side: List of application handshake protocols of the EV */
+			sprintf(s, "Vehicle supports %d protocols. ", exiDocAppHand.supportedAppProtocolReq.AppProtocol.arrayLen);
+			strcat(gResultString, s);
+
+			for(i=0;i<exiDocAppHand.supportedAppProtocolReq.AppProtocol.arrayLen;i++) {
+				sprintf(s, "ProtocolEntry#%d ",(i+1));
+				strcat(gResultString, s);
+				sprintf(s, "ProtocolNamespace=");
+				strcat(gResultString, s);
+				printASCIIString(exiDocAppHand.supportedAppProtocolReq.AppProtocol.array[i].ProtocolNamespace.characters, exiDocAppHand.supportedAppProtocolReq.AppProtocol.array[i].ProtocolNamespace.charactersLen);
+				strcat(gResultString, s);
+				sprintf(s, " Version=%d.%d ", exiDocAppHand.supportedAppProtocolReq.AppProtocol.array[i].VersionNumberMajor, exiDocAppHand.supportedAppProtocolReq.AppProtocol.array[i].VersionNumberMinor);
+				strcat(gResultString, s);
+				sprintf(s, "SchemaID=%d ", exiDocAppHand.supportedAppProtocolReq.AppProtocol.array[i].SchemaID);
+				strcat(gResultString, s);
+				sprintf(s, "Priority=%d ", exiDocAppHand.supportedAppProtocolReq.AppProtocol.array[i].Priority);
+				strcat(gResultString, s);
+			}
+		}
+		if (exiDocAppHand.supportedAppProtocolRes_isUsed) {
+			/* it is a response */
+			sprintf(gResultString, "ResponseCode %d, SchemaID_isUsed %d, SchemaID %d",
+				exiDocAppHand.supportedAppProtocolRes.ResponseCode,
+				exiDocAppHand.supportedAppProtocolRes.SchemaID_isUsed,
+				exiDocAppHand.supportedAppProtocolRes.SchemaID);
+			//if (exiDocAppHand.supportedAppProtocolRes.SchemaID_isUsed) {
+			//	SchemaID
+			//}	
 		}
 	} else {
 		sprintf(gErrorString, "Schema not implemented.");
